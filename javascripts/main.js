0ebce1230931eaa983d7f5edf4b84bf5e7df69dd
Todd Spainhour <toddspainhour@gmail.com>
2020-09-30 19:30:17 -0500

    added calculator buttons and event listeners

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log("Your javascript file is connected.");
+
+const clicked = () => {
+  console.log("You clicked a button")
+};
+
+
+let button = document.getElementsByClassName("calculator-button");
+
+console.log(`there are ${button.length} button elements.`)
+
+for (let i = 0; i < button.length; i++)
+{
+
+  button[i].addEventListener("click", clicked)
+}
