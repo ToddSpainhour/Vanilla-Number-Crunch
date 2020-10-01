f7e422105719dad4df52346d9ac3e724ca849a08
Todd Spainhour <toddspainhour@gmail.com>
2020-09-30 22:35:35 -0500

    numbers display and backspace button works

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,41 @@
 
-const clicked = () => {
-  console.log("You clicked a button")
+let userEntry = [];
+
+
+
+
+
+
+const clickedButtonEvent = (e) => {
+  e.preventDefault();
+  if (e.target.value  >= 0 && e.target.value <= 9) 
+  {
+      userEntry.push(e.target.value)
+      document.getElementById('displayEntries').innerHTML = userEntry.join('');
+      console.log(`userEntry array is: ${userEntry}`)
+  } 
+  else if (e.target.value = "backspace")
+  {
+      console.log("inside your else if backspace btn clicked")
+  }
 };
 
 
-let button = document.getElementsByClassName("calculator-button");
-
-console.log(`there are ${button.length} button elements.`)
-
-for (let i = 0; i < button.length; i++)
+const backspaceButtonEvent = (e) => 
 {
+  console.log("You clikde the backspace btn");
+  userEntry.splice(userEntry.length -1);
+  document.getElementById('displayEntries').innerHTML = userEntry.join('');
+  console.log(`userEntry array is: ${userEntry}`)
+}
 
-  button[i].addEventListener("click", clicked)
+
+
+let numberButton = document.getElementsByClassName("number-button");
+let backspaceButton = document.getElementById("backspaceButton");
+backspaceButton.addEventListener("click", backspaceButtonEvent)
+
+for (let i = 0; i < numberButton.length; i++)
+{
+  numberButton[i].addEventListener("click", clickedButtonEvent)
 }
